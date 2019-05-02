6b2acc5a1369a1dbbec04f72876fc7a5085c7364
dinoLLm <dinolm@hawaii.edu>
2019-05-01 20:25:51 -1000

    merge from dino

---
+++ tst/tileType_test.cpp
@@ -0,0 +1,41 @@
+#include "main_menu.hpp"
+#include "action.hpp"
+#include "map.hpp"
+
+
+int main() {
+
+     //Create the window
+//     RenderWindow window(VideoMode(800, 600), "TEST");
+//     Event event;
+
+     //Initialize objects needed for game state (always starts at Main Menu)
+     Map map;
+     Game game;
+     game.pushState(new Main_Menu(&game));
+
+     //Run until window closes
+     while(game.window.isOpen()) {
+          //Read event
+          while(game.window.pollEvent(game.event)) 
+          {
+
+               game.getState()->handleInput();
+
+          }
+
+
+          map.setSpawn(20);
+          map.square[20]->setTileType(forest);
+          std::cout << "Square 20 is now forest type" << std::endl;
+
+
+          //Update graphics accordingly
+          game.getState()->update();
+          //Redraw screen
+          game.getState()->draw();
+          //Push buffer to screen
+          game.window.display();
+
+     }
+}
