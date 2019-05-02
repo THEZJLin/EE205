6e2a7dfe3e4c8650617935bfc40f935c95cc1dae
dinoLLm <dinolm@hawaii.edu>
2019-05-01 21:41:37 -1000

    Defualted all map squares to tileType forest, tile type now changes to a color depending on terrain (not working)

--- src/map.cpp
+++ src/map.cpp
@@ -3,12 +3,13 @@
 //Constructor, creates new squares and pushes them into the map vector
 Map::Map() {
      n = 0;
-     for(int i=0; i<MAP_DIM;++i) {
-          for(int j=0; j<MAP_DIM; ++j) {
+     for(int i=0; i<MAP_DIM; i++) {
+          for(int j=0; j<MAP_DIM; j++) {
                //Coordinates of new square are modified by the "SIZE"
                //alias defined in square.cpp
                square.push_back(new Square(i*SIZE,j*SIZE,n));
-               
+               //Defualts all of the map tiles to have forest terrain
+               (*(square.rbegin()))->setTileType(forest);
                //n is incremented to avoid 2 squares having the same
                //identifier
                n++;
